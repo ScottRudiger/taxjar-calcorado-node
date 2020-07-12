df5a74da94f9b5831e7dd642a105cbbb87eca6fb
ScottRudiger <scottrudiger@gmail.com>
2020-07-12 16:50:27 -0700

    Provide default value when `nexus_addresses` doesn't exist in original request

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = (apiReq) => {
   let toCity = apiReq.to_city.toUpperCase();
-  let nexusAddresses = apiReq.nexus_addresses;
+  let nexusAddresses = apiReq.nexus_addresses || [];
 
   nexusAddresses.forEach((address, idx, array) => {
     if (toCity.localeCompare(address.city.toUpperCase()) == 0) {
