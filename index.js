d13a4ba5bbbb37277d738616705aff58149f7b7a
ScottRudiger <scottrudiger@gmail.com>
2020-07-12 17:25:25 -0700

    Refactor from `forEach` to `for of` loop
    `Array.prototype.forEach` always returns `undefined`. Refactoring to a `for of` loop allows the modified API request to be returned from the module

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 module.exports = (apiReq) => {
   let toCity = apiReq.to_city.toUpperCase();
   let nexusAddresses = apiReq.nexus_addresses || [];
+  let idx = 0;
 
-  nexusAddresses.forEach((address, idx, array) => {
+  for (const address of nexusAddresses) {
     if (toCity.localeCompare(address.city.toUpperCase()) == 0) {
       apiReq.nexus_addresses = address;
       return apiReq
     }
-    if (idx === array.length - 1 && apiReq.nexus_addresses.length > 1) {
+    if (idx++ === nexusAddresses.length - 1 && apiReq.nexus_addresses.length > 1) {
       delete apiReq.nexus_addresses;
       return apiReq
     }
-  });
+  }
 
 }
