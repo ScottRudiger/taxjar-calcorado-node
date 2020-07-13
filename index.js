061e24cf1d643414594e2c121f723d1631fce13e
ScottRudiger <scottrudiger@gmail.com>
2020-07-12 20:56:44 -0700

    Add `useTaxJarNexusFallback` configuration option and avoid mutation of original request
    When a nexus match cannot be found at the city level, this option affects calcorado's behavior. Defaults to false; when false, use the full list of nexus addresses passed to the original request as `nexus_addresses`; when true, throw out the passed in `nexus_addresses` and fall back to the nexus addresses set up in the merchant's TaxJar account.
    
    Also, rather than replacing/deleting `nexus_addresses` in the original request, using `Object.assign` this ways allows us to return a new tax request object with altered `nexus_addresses` without mutating the original object.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-module.exports = (apiReq) => {
+module.exports = (apiReq, options = {}) => {
   let toCity = (apiReq.to_city || '').toUpperCase();
   let nexusAddresses = apiReq.nexus_addresses || [];
-  let idx = 0;
 
   for (const address of nexusAddresses) {
     if (toCity.localeCompare(address.city && address.city.toUpperCase()) == 0) {
-      apiReq.nexus_addresses = [address];
-      return apiReq
-    }
-    if (idx++ === nexusAddresses.length - 1 && apiReq.nexus_addresses.length > 1) {
-      delete apiReq.nexus_addresses;
-      return apiReq
+      return Object.assign({}, apiReq, {nexus_addresses: [address]});
     }
   }
 
-}
+  if (options.useTaxJarNexusFallback) {
+    return Object.assign({}, apiReq, {nexus_addresses: null});
+  }
+
+  return apiReq;
+};
