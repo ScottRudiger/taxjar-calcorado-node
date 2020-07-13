8d149da5687fa6e4d0237d45697b50a6d210519d
ScottRudiger <scottrudiger@gmail.com>
2020-07-12 22:56:08 -0700

    Check for ship-to state match in addition to city
    This is to avoid false-positives for identical city names in different states

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 module.exports = (apiReq, options = {}) => {
-  let toCity = (apiReq.to_city || '').toUpperCase();
-  let nexusAddresses = apiReq.nexus_addresses || [];
+  const toState = (apiReq.to_state || '').toUpperCase();
+  const toCity = (apiReq.to_city || '').toUpperCase();
+  const nexusAddresses = apiReq.nexus_addresses || [];
 
   for (const address of nexusAddresses) {
-    if (toCity.localeCompare(address.city && address.city.toUpperCase()) == 0) {
+    const cityMatch = toCity.localeCompare(address.city && address.city.toUpperCase()) == 0;
+    const stateMatch = toState.localeCompare(address.state && address.state.toUpperCase()) == 0;
+    if (cityMatch && stateMatch) {
       return Object.assign({}, apiReq, {nexus_addresses: [address]});
     }
   }
